b52cb7b1588b42d087e6df7c4ae1a99cb6289731
William Bennett <williamgbennett1@gmail.com>
2024-11-08 20:37:14 -0700

    crc

--- converging_test4.py
+++ converging_test4.py
@@ -73,7 +73,7 @@ MM = 1
 N_ang = 8
 # N_spaces_list = [10, 15, 20, 25, 50, 75, 100, 150, 200, 500, 1000]
 # N_spaces_list = [10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-N_spaces_list = [270]
+N_spaces_list = [500]
 
 run = run()
 run.load()
@@ -100,10 +100,10 @@ for it, N_space in enumerate(N_spaces_list):
     run.parameters['all']['Ms'] = [MM]
     run.mesh_parameters['Msigma'] = MM
 
-    menis_times = np.array([-94.706889, -27.126998, -1])
+    # menis_times = np.array([-94.706889, -27.126998, -1])
     # menis_times = np.array([-1])
     # menis_times = np.array([-140, -94.706889, -27.126998])
-    # menis_times = np.array([-140, -100, -94.706889])
+    menis_times = np.array([-140, -100, -94.706889])
     # menis_times = np.array([-145, -140, -135])
     # menis_times = np.array([-145, -144, -143])
 
